eae8f281c905d1c3fdf69b0b551b0503a52fac1e
aryans41 <aryans41@nycstudents.net>
2024-10-01 16:14:43 +0000

    second commit

--- src/main/java/com/example/project/StudentRunner.java
+++ src/main/java/com/example/project/StudentRunner.java
@@ -1,5 +1,8 @@
+package com.example.project;
+
 public class StudentRunner{
     public static void main(String[] args) {
-        //use this StudentRunner class if you need to test your code 
+        Student s1 = new  Student("Sean","Rosenstein",2026);
+        s1.printStudentInfo();
     }
 }
